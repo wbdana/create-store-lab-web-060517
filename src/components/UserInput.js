48466e650626c8dcc1cc0277c2c953dee3323b33
wbdana <william.b.dana@gmail.com>
2017-08-29 13:33:19 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  state = {
+    username: '',
+    hometown: ''
+  }
+
+  handleUsernameChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleHometownChange = (event) => {
+    this.setState({
+      hometown: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        username: this.state.username,
+        hometown: this.state.hometown
+      }
+    })
+  }
+
+
   render() {
     return(
-      <div>
-        user input
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' onChange={this.handleUsernameChange} />
+        <input type='text' onChange={this.handleHometownChange} />
+        <input type='submit' />
+      </form>
     );
   }
 };
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,8 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = { users: [] }, action){
+  switch (action.type) {
+    case 'ADD_USER':
+      return Object.assign({}, state, { users: [...state.users, action.user] })
+    default:
+      return state
+  }
 }
